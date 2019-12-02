47cecd83b419a86a487e33a9644e48d9095d6422
YANSEN.XU <xys8804@gmail.com>
2019-12-02 14:22:58 +0900

    add an SFC example packages

---
+++ sfc_examples/sfc1.py
@@ -0,0 +1,17 @@
+sfc = {
+    "name": "sfc_1",
+    "type": "xx",
+    "vnf_list": [
+        {"type": 2, "name": "vnf1", "CPU": 10},
+        {"type": 2, "name": "vnf2", "CPU": 20},
+        # {"type": 2, "name": "vnf3", "CPU": 30},
+        # {"type": 2, "name": "vnf4", "CPU": 40},
+        # {"type": 2, "name": "vnf5", "CPU": 40},
+
+    ],
+    "bandwidth": 30,
+    "src_node": 1,
+    "dst_node": 4,
+    "latency": 10,
+    "duration": 20
+}
