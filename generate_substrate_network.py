0feb3b4bf90e8582ac4a92b0770bdf3c251da4db
Yansen XU <xys8804@gmail.com>
2019-12-02 20:50:38 +0900

    change substrate network to substrate random network to avoid name conflict

--- generate_substrate_network.py
+++ generate_substrate_network.py
@@ -6,9 +6,9 @@ from core.vnf import VNF
 import random
 
 # create a new substrate network
-substrate_network = Net()
+substrate_random_network = Net()
 
-number_of_nodes = 50
+number_of_nodes = 100
 probability = 0.2
 topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
 network_create_counter = 0
@@ -21,17 +21,17 @@ while not nx.is_connected(topology):
 
 for edge in topology.edges():
     bw = random.randint(50, 100)
-    substrate_network.init_bandwidth_capacity(edge[0], edge[1], bw)
+    substrate_random_network.init_bandwidth_capacity(edge[0], edge[1], bw)
     lt = random.uniform(1, 5)
-    substrate_network.init_link_latency(edge[0], edge[1], lt)
+    substrate_random_network.init_link_latency(edge[0], edge[1], lt)
 
 for node in topology.nodes():
     cpu_capacity = random.randint(50, 100)
-    substrate_network.init_node_cpu_capacity(node, cpu_capacity)
-
-substrate_network.update()
-substrate_network.print_out_nodes_information()
-substrate_network.print_out_edges_information()
+    substrate_random_network.init_node_cpu_capacity(node, cpu_capacity)
+substrate_random_network.pre_get_single_source_minimum_latency_path()
+substrate_random_network.update()
+substrate_random_network.print_out_nodes_information()
+substrate_random_network.print_out_edges_information()
 
 
 
