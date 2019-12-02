3216dc9ed8a2db2a202050007a8f674cf154e548
YANSEN.XU <xys8804@gmail.com>
2019-12-02 14:27:52 +0900

    [k shortest algorithm] for comparison, not yet implemented

---
+++ algorithms/k_shortest_algorithm.py
@@ -0,0 +1,109 @@
+import copy
+import random
+
+"""
+Consideration:
+Algorithm should not do any modification on Substrate network/
+
+It should only use network information and sfc information to 
+solve and give out a mapping and route info, that
+
+sfc_id: 
+{substrate_node_id <- vnf_id}
+<- is a mapping
+
+sfc_id:
+{
+    src:  [1, 2, 3],
+    vnf1: [3, 4, 5],
+    vnf2: [5, 6, 7],
+    vnf3: [7, 8 ,9],
+    dst:  []
+
+}
+
+
+"""
+import logging
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+# ch = logging.StreamHandler()
+ch = logging.FileHandler('./logs/KShortestAlgorithm.log')
+ch.setLevel(logging.DEBUG)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# add formatter to ch
+ch.setFormatter(formatter)
+# add ch to logger
+logger.addHandler(ch)
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
+
+
+
+
+class KShortestAlgorithm():
+    def __init__(self):
+        self.substrate_network = None
+        self.sfc = None
+        self.node_info = None
+        self.route_info = None
+        self.latency = None
+
+
+    def clear_all(self):
+        print "k shortest algorithm: clear all"
+        self.substrate_network = None
+        self.sfc = None
+        self.node_info = None
+        self.route_info = None
+        self.latency = None
+
+    def install_substrate_network(self, substrate_network):
+        self.substrate_network = copy.deepcopy(substrate_network)
+        return self.substrate_network
+
+    def install_SFC(self, sfc):
+        self.sfc = sfc
+        return self.sfc
+
+    def start_algorithm(self):
+        substrate_network = self.substrate_network
+        sfc = self.sfc
+        self.algorithm(substrate_network, sfc)
+        return
+
+    def get_latency(self):
+        return self.latency
+    
+    def get_route_info(self):
+        return self.route_info
+
+    def algorithm(self, substrate_network, sfc):
+    
+        print "k shortest algorithm: "
+
+        nodes = substrate_network.nodes()
+        ## Get src and dst vnf
+        src_vnf = sfc.get_src_vnf()
+        dst_vnf = sfc.get_dst_vnf()
+
+        ## Get substrate network nodes that src and dst are assigned in advanced
+        src_substrate_node = sfc.get_substrate_node(src_vnf)
+        dst_substrate_node = sfc.get_substrate_node(dst_vnf)
+
+        route_info = {}
+        number_of_vnfs = sfc.get_number_of_vnfs()
+        current_vnf = src_vnf
+        current_substrate_node = src_substrate_node
+
+        used_node = [src_substrate_node, dst_substrate_node]
+       
