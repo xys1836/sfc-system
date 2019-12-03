452d9ec1038ab5405402591ce2941f3b741736d6
Yansen XU <xys8804@gmail.com>
2019-12-03 18:41:34 +0900

    Add a function for generate a random substrate network

--- generate_substrate_network.py
+++ generate_substrate_network.py
@@ -34,4 +34,31 @@ substrate_random_network.print_out_nodes_information()
 substrate_random_network.print_out_edges_information()
 
 
+def generate_random_network():
+    # create a new substrate network
+    substrate_random_network = Net()
+
+    number_of_nodes = 100
+    probability = 0.2
+    topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
+    network_create_counter = 0
+    while not nx.is_connected(topology):
+        if network_create_counter >= 10000:
+            break
+        network_create_counter += 1
+        topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
+
+    for edge in topology.edges():
+        bw = random.randint(50, 100)
+        substrate_random_network.init_bandwidth_capacity(edge[0], edge[1], bw)
+        lt = random.uniform(1, 5)
+        substrate_random_network.init_link_latency(edge[0], edge[1], lt)
+
+    for node in topology.nodes():
+        cpu_capacity = random.randint(50, 100)
+        substrate_random_network.init_node_cpu_capacity(node, cpu_capacity)
+    substrate_random_network.pre_get_single_source_minimum_latency_path()
+    substrate_random_network.update()
+    return substrate_random_network
+
 
