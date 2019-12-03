7e89f5194d895e540fdb813905107ba050ac069b
Yansen XU <xys8804@gmail.com>
2019-12-03 18:49:07 +0900

    reconstruct simple substrate network topology

--- topology/simple_substrate_network.py
+++ topology/simple_substrate_network.py
@@ -4,7 +4,7 @@ from core.net import Net
 from core.sfc import SFC
 from core.vnf import VNF
 import random
-
+from utils.k_shortest_paths import k_shortest_paths
 simple_six_node_topology = None
 
 
@@ -25,12 +25,12 @@ def generate_substrate_network():
     substrate_network.init_link_latency(1, 6, 2)
     substrate_network.init_link_latency(1, 2, 1)
     substrate_network.init_link_latency(2, 3, 4)
-    substrate_network.init_link_latency(3, 4, 3)
+    substrate_network.init_link_latency(3, 4, 30)
     substrate_network.init_link_latency(4, 5, 2)
     substrate_network.init_link_latency(5, 6, 2)
     substrate_network.init_link_latency(2, 6, 5)
     # substrate_network.init_link_latency(2, 5, 2)
-    substrate_network.init_link_latency(3, 5, 3)
+    substrate_network.init_link_latency(3, 5, 30)
 
     substrate_network.init_node_cpu_capacity(1, 100)
     substrate_network.init_node_cpu_capacity(2, 100)
@@ -42,8 +42,3 @@ def generate_substrate_network():
     return substrate_network
 
 simple_six_node_topology = generate_substrate_network()
-
-
-
-
-
