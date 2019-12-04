5ba144bedeb067eb4aa2277a1abe5c9038cfdb1c
Yansen XU <xys8804@gmail.com>
2019-12-04 21:38:03 +0900

    [generate substrate network] remove raw code line

--- generate_substrate_network.py
+++ generate_substrate_network.py
@@ -4,42 +4,42 @@ from core.net import Net
 from core.sfc import SFC
 from core.vnf import VNF
 import random
-
+import matplotlib.pyplot as plt
 # create a new substrate network
-substrate_random_network = Net()
+# substrate_random_network = Net()
+#
+# number_of_nodes = 100
+# probability = 1.0
+# topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
+# network_create_counter = 0
+# while not nx.is_connected(topology):
+#     if network_create_counter >= 10000:
+#         break
+#     network_create_counter += 1
+#     topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
+#
+#
+# for edge in topology.edges():
+#     bw = random.randint(50, 100)
+#     substrate_random_network.init_bandwidth_capacity(edge[0], edge[1], bw)
+#     lt = random.uniform(1, 5)
+#     substrate_random_network.init_link_latency(edge[0], edge[1], lt)
+#
+# for node in topology.nodes():
+#     cpu_capacity = random.randint(50, 100)
+#     substrate_random_network.init_node_cpu_capacity(node, cpu_capacity)
+# substrate_random_network.pre_get_single_source_minimum_latency_path()
+# substrate_random_network.update()
+# substrate_random_network.print_out_nodes_information()
+# substrate_random_network.print_out_edges_information()
 
-number_of_nodes = 100
-probability = 0.2
-topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
-network_create_counter = 0
-while not nx.is_connected(topology):
-    if network_create_counter >= 10000:
-        break
-    network_create_counter += 1
-    topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
 
-
-for edge in topology.edges():
-    bw = random.randint(50, 100)
-    substrate_random_network.init_bandwidth_capacity(edge[0], edge[1], bw)
-    lt = random.uniform(1, 5)
-    substrate_random_network.init_link_latency(edge[0], edge[1], lt)
-
-for node in topology.nodes():
-    cpu_capacity = random.randint(50, 100)
-    substrate_random_network.init_node_cpu_capacity(node, cpu_capacity)
-substrate_random_network.pre_get_single_source_minimum_latency_path()
-substrate_random_network.update()
-substrate_random_network.print_out_nodes_information()
-substrate_random_network.print_out_edges_information()
-
-
-def generate_random_network():
+def generate_random_network(number_of_node, probability):
     # create a new substrate network
     substrate_random_network = Net()
 
-    number_of_nodes = 100
-    probability = 0.2
+    number_of_nodes = number_of_node
+    probability = probability
     topology = nx.erdos_renyi_graph(number_of_nodes, probability, seed=None, directed=False)
     network_create_counter = 0
     while not nx.is_connected(topology):
@@ -61,4 +61,10 @@ def generate_random_network():
     substrate_random_network.update()
     return substrate_random_network
 
+if __name__ == '__main__':
+    substrate_random_network = generate_random_network(10, 0.1)
+    bc = nx.algorithms.centrality.betweenness_centrality(substrate_random_network, weight='latency')
 
+    nx.draw(substrate_random_network)
+    plt.show()
+    print bc
